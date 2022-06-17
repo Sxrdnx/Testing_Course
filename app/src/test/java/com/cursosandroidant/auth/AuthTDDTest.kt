31c88feb59b82377dcac7a89a1e1281ff9e3c9cd
Andres <andres.diaz@corusconsulting.com>
2022-06-16 22:31:36 -0500

    change name of test with rule given-when-then and add hamcrest

--- app/src/test/java/com/cursosandroidant/auth/AuthTDDTest.kt
+++ app/src/test/java/com/cursosandroidant/auth/AuthTDDTest.kt
@@ -7,72 +7,70 @@ import org.junit.Test
 import java.lang.NullPointerException
 
 class AuthTDDTest {
-
-
+   //given-when-then -> regla para nombrar tests
     @Test
-    fun login_completeFrom_existUser_returnsSuccessEvent(){
+    fun loginUser_correctData_returnsSuccessEvent(){
         val isAuthenticated = userAuthenticationTDD("ant@gmail.com","1234")
         assertThat(isAuthenticated).isEqualTo(USER_EXIST)
     }
 
     @Test
-    fun login_completeForm_notExistUser_returnsFailEvent(){
+    fun loginUser_wrongData_returnsFailEvent(){
         val isAuthenticated = userAuthenticationTDD("ola@gmail.com","1234")
         assertThat(isAuthenticated).isEqualTo(NOT_USER_EXIST)
     }
 
     @Test
-    fun login_emptyEmail_returnsFailEvent(){
+    fun loginUser_emptyEmail_returnsFailEvent(){
         val isAuthenticated = userAuthenticationTDD("","1234")
         assertThat(isAuthenticated).isEqualTo(EMPTY_EMAIL)
     }
 
     @Test
-    fun login_emptyPassword_returnsFailEvent(){
+    fun loginUser_emptyPassword_returnsFailEvent(){
         val isAuthenticated = userAuthenticationTDD("ant@gmail.com" ,"")
         assertThat(isAuthenticated).isEqualTo(EMPTY_PASSWORD)
     }
 
     @Test
-    fun login_emptyForm_returnsFailEvent(){
+    fun loginUser_emptyForm_returnsFailEvent(){
         val isAuthenticated = userAuthenticationTDD("" ,"")
         assertThat(isAuthenticated).isEqualTo(EMPTY_FORM)
     }
 
     @Test
-    fun login_completeForm_invalidEmail_returnsFailEvent(){
+    fun loginUser_invalidEmail_returnsFailEvent(){
         val isAuthenticated = userAuthenticationTDD("ant@gmailcom" ,"1234")
         assertThat(isAuthenticated).isEqualTo(INVALID_EMAIL)
     }
 
     @Test
-    fun login_completeForm_invalidPassword_returnsFailEvent(){
+    fun loginUser_invalidPassword_returnsFailEvent(){
         val isAuthenticated = userAuthenticationTDD("ant@gmail.com" ,"123W")
         assertThat(isAuthenticated).isEqualTo(INVALID_PASSWORD)
-
     }
 
     @Test
-    fun login_completeForm_invalidUser_returnsFailEvent(){
+    fun loginUser_invalidUser_returnsFailEvent(){
         val isAuthenticated = userAuthenticationTDD("ant@gmailcom" ,"123W")
         assertThat(isAuthenticated).isEqualTo(INVALID_USER)
     }
 
     @Test(expected = AuthException::class)
-    fun login_nullEmail_returnsException(){
+    fun loginUser_nullEmail_returnsAuthException(){
         val isAuthenticated = userAuthenticationTDD(null ,"123W")
         assertThat(isAuthenticated).isEqualTo(NULL_EMAIL)
     }
 
     @Test
-    fun login_nullPassword_returnsException(){
+    fun loginUSer_nullPassword_returnsAuthException(){
         assertThrows(AuthException::class.java){
             println(userAuthenticationTDD( "ant@gmailcom",null))
         }
     }
 
     @Test
-    fun login_nullForm_returnsException(){
+    fun loginUser_nullData_returnsAuthException(){
         try {
             val result = userAuthenticationTDD(null ,null)
             assertThat(NULL_FORM).isEqualTo(result)
@@ -84,7 +82,7 @@ class AuthTDDTest {
     }
 
     @Test
-    fun login_completeForm_errorLengthPassword_returnsFailEvent(){
+    fun loginUser_errorLengthPassword_returnsFailEvent(){
         val result = userAuthenticationTDD("ant@gmail.com" ,"12345")
         assertThat(result).isEqualTo(LENGTH_PASSWORD_INVALID)
     }
